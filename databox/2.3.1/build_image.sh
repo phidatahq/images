a0bf7ff9eaefb12065523c660c772c9d966270d3
Ashpreet Bedi <ashpreet@phidata.com>
2022-06-02 23:14:59 -0400

    Auto commit on Thu Jun  2 23:14:59 EDT 2022

---
+++ databox/2.3.1/build_image.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CURR_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+REPO="phidata"
+NAME="databox"
+TAG="2.3.1"
+
+echo "Running: docker build -t $REPO/$NAME:$TAG $CURR_SCRIPT_DIR"
+docker build -t $REPO/$NAME:$TAG $CURR_SCRIPT_DIR
+docker push $REPO/$NAME:$TAG
