0a95032e2b9a32d86c23f0100251c6d496851a40
Ashpreet Bedi <ashpreet@phidata.com>
2022-09-26 16:30:58 -0400

    airflow 2.4.0

---
+++ airflow/2.4.0/build_image.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+CURR_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+REPO="phidata"
+NAME="airflow"
+TAG="2.4.0"
+
+echo "Running: docker build -t $REPO/$NAME:$TAG $CURR_SCRIPT_DIR"
+docker build -t $REPO/$NAME:$TAG $CURR_SCRIPT_DIR
+docker push $REPO/$NAME:$TAG
