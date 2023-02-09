bc144702f632f82693cf9cd4b23e4d766124d97c
Ashpreet Bedi <ashpreet@phidata.com>
2023-02-08 20:24:34 -0500

    update

--- airflow-spark/2.5.1/build_image.sh
+++ airflow-spark/2.5.1/build_image.sh
@@ -8,5 +8,5 @@ NAME="airflow-spark"
 TAG="2.5.1"
 
 # Run docker buildx create --use before running this script
-echo "Running: docker buildx build --platform=linux/amd64,linux/arm64 -t $REPO/$NAME:$TAG $CURR_SCRIPT_DIR"
-docker buildx build --platform=linux/amd64,linux/arm64 -t $REPO/$NAME:$TAG $CURR_SCRIPT_DIR --push
+echo "Running: docker buildx build --platform=linux/amd64 -t $REPO/$NAME:$TAG $CURR_SCRIPT_DIR"
+docker buildx build --platform=linux/amd64 -t $REPO/$NAME:$TAG $CURR_SCRIPT_DIR --push
